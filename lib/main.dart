f40318e1517379cfaa9ecb3f11a53f88ed560150
zain-62 <zainkhan6253@gmail.com>
2024-02-11 23:20:54 +0500

    firebase added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/screens/main_screens/loginscreen.dart';
 
-void main(List<String> args) {
+void main(List<String> args) async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+    apiKey: 'AIzaSyAa20EcWQ0GUEUcVvrSZYxyh_6oPspU3yQ',
+    appId: '1:823024169394:android:5d567a4af05c2c1cbbb4ab',
+    messagingSenderId: '823024169394',
+    projectId: 'lms-firebase-c58dc',
+    storageBucket: 'lms-firebase-c58dc.appspot.com',
+  ));
+
   runApp(const MyApp());
 }
 
