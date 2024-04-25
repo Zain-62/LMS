f8e43f650259b0ae105e0d04ebb8903c8b8efd2f
zain-62 <zainkhan6253@gmail.com>
2024-04-25 21:49:05 +0500

    code optimize

--- lib/screens/functional_screens/assignment_screen.dart
+++ lib/screens/functional_screens/assignment_screen.dart
@@ -1,8 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
-import 'package:fluttertoast/fluttertoast.dart';
-import 'package:gap/gap.dart';
 
 import '../../utilities/constants.dart';
 import '../../utilities/textdetail.dart';
